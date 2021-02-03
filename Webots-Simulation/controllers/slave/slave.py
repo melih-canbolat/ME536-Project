3653ae6cec6eec8d049e5cab383b668583ba49d2
melih-canbolat <melih.canbolat@gmail.com>
2021-02-04 01:38:56 +0300

    Reqular Update to robot controller

--- Webots-Simulation/controllers/slave/slave.py
+++ Webots-Simulation/controllers/slave/slave.py
@@ -1,4 +1,5 @@
-from controller import Robot
+from controller import Robot, Receiver, Display, Camera
+import numpy as np
 
 
 class Slave (Robot):
@@ -9,22 +10,40 @@ class Slave (Robot):
 
     def __init__(self):
         super(Slave, self).__init__()
-        
-        self.camera = self.getDevice('camera')
-        self.camera.enable(1 * self.timeStep)
-        
         self.receiver = self.getDevice('receiver')
         self.receiver.enable(self.timeStep)
+        self.display = self.getDevice('display')
+        self.camera = self.getDevice('camera')
+        self.camera.enable(2 * self.timeStep)
 
+    def retrieveImage(self):  # Capture an image and return it as grayscale image
+        image = self.camera.getImageArray()  # Get image from the camera as a nested list
+        if image is None: return None  # Camera may return None at the beginning
+        img = np.array(image)  # Convert to a numpy array
+        gimg = np.dot(img[...,:3], [0.2989, 0.5870, 0.1140])  # Convert image to grayscale
+        return gimg
+    
+    def displayImage(self, gray):  # Display grayscale image on 3D View
+        if gray is None: return  # Don't proceed if the image is empty
+        gimg = np.stack((gray,)*3, axis=-1)
+        ir = self.display.imageNew(gimg.tolist(), Display.RGB, 200, 200)
+        self.display.imagePaste(ir, 0, 0)
+        self.display.imageDelete(ir)
+        
     def run(self):
         while True:
-            # Read the supervisor order.
-            if self.receiver.getQueueLength() > 0:
+            if self.receiver.getQueueLength() > 0:  # Read the supervisor order.
                 message = self.receiver.getData().decode('utf-8')
                 self.receiver.nextPacket()
                 if message == 'avoid obstacles':
                     pass
+            image = self.retrieveImage()
+            #self.displayImage(image)
+            
+            
+            # Send "image" appropriate medium to process.
 
+        
             # Perform a simulation step, quit the loop when
             # Webots is about to quit.
             if self.step(self.timeStep) == -1:
