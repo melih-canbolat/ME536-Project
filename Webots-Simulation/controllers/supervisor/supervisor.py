4525ddfb41f2560a087bf0aee4b77dae1f1b4786
melih-canbolat <melih.canbolat@gmail.com>
2021-02-04 13:38:16 +0300

    Various improvements to supervisor controller

--- Webots-Simulation/controllers/supervisor/supervisor.py
+++ Webots-Simulation/controllers/supervisor/supervisor.py
@@ -15,49 +15,45 @@ class Driver (Supervisor):
 
     def run(self):
         print("Simulation has started.")
-        previous_message = ''
+        print("S: Save the current image")
+        print("P: Display the current position")
+
 
         # Main loop.
         while True:
-            # Deal with the pressed keyboard key.
-            k = self.keyboard.getKey()
             message = ''
-
-            if k == 314:  # Left
-                print("left")
+            k = self.keyboard.getKey()  # Get the pressed keyboard key.
+            #print(k)
+            if k == 314:  # Move Left
                 self.translation[0] -= 0.01
                 self.translationField.setSFVec3f(self.translation)
                 
-            elif k == 315:  # Up
-                print("up")
+            elif k == 315:  # Move Up
                 self.translation[2] -= 0.01
                 self.translationField.setSFVec3f(self.translation)
 
-            elif k == 316:  # Right
-                print("Right")
+            elif k == 316:  # Move Right
                 self.translation[0] += 0.01
                 self.translationField.setSFVec3f(self.translation)
 
-            elif k == 317:  # Down
-                print("down")
+            elif k == 317:  # Move Down
                 self.translation[2] += 0.01
                 self.translationField.setSFVec3f(self.translation)
-
-            elif k == ord('A'):
-                message = 'avoid obstacles'
+            elif k == ord('S'): # S - save image
+                message = "capture"
             elif k == ord('P'):  # Get translation information of the robot
                 translationValues = self.translationField.getSFVec3f()
                 print(translationValues)
-            elif k == ord('R'):  # Teleport the robot to "self.translation"
-                self.translationField.setSFVec3f(self.translation)
 
-            # Send a new message through the emitter device.
-            if message != '' and message != previous_message:
-                previous_message = message
+                
+            # Deternmine the new message to be sent
+            # Send a new message to the RobotCam through the emitter device.
+            if message != "":
                 self.emitter.send(message.encode('utf-8'))
+                message = ""
 
-            # Perform a simulation step, quit the loop when
-            # Webots is about to quit.
+            # Perform a simulation step, 
+            # quit the loop when Webots is about to quit.
             if self.step(self.timeStep) == -1:
                 break
 
